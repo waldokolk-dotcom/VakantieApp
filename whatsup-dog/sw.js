43c9cc50241e51f9ab7cf6f87708e164bc0023f4
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-08 15:15:42 +0200

    Ververs PWA-cache voor woonplaats-onboarding

--- whatsup-dog/sw.js
+++ whatsup-dog/sw.js
@@ -1,5 +1,5 @@
-const CACHE='whatsup-dog-v1';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='whatsup-dog-v2';
+const CORE=['./','./index.html','./styles.css','./app.js','./official-areas.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{if(new URL(e.request.url).origin===location.origin){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone))}return res}).catch(()=>caches.match('./index.html'))))});
