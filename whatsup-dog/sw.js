460a713ce95033b4187a527742c46311cc2bf847
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-08 22:47:10 +0200

    Cache Waldo mark in Whatsup dog PWA

--- whatsup-dog/sw.js
+++ whatsup-dog/sw.js
@@ -1,5 +1,5 @@
-const CACHE='whatsup-dog-v5-home-gis';
-const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg'];
+const CACHE='whatsup-dog-v6-waldo-mark';
+const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg','./waldo-mark.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
