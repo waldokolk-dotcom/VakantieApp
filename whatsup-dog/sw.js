7ecb48cffeb6c4a05d4b69f9dd9fa09d1045c209
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-09 10:52:13 +0200

    Cache digitized Nijkerk off-leash GeoJSON

--- whatsup-dog/sw.js
+++ whatsup-dog/sw.js
@@ -1,5 +1,5 @@
-const CACHE='whatsup-dog-v7-nijkerk-only';
-const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg','./waldo-mark.png'];
+const CACHE='whatsup-dog-v8-nijkerk-geojson';
+const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg','./waldo-mark.png','./data/nijkerk-losloopgebieden.geojson'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
