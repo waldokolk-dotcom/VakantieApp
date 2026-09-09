6bf2e48adb6430834530d4a028cad2207588cca5
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-09 10:54:17 +0200

    Refresh Whatsup dog cache for digitized Nijkerk map

--- whatsup-dog/sw.js
+++ whatsup-dog/sw.js
@@ -1,4 +1,4 @@
-const CACHE='whatsup-dog-v8-nijkerk-geojson';
+const CACHE='whatsup-dog-v9-nijkerk-digitized';
 const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg','./waldo-mark.png','./data/nijkerk-losloopgebieden.geojson'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
