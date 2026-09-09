58e9a5da21703ded7efba55ec15c3848a99f60a3
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-09 11:26:29 +0200

    Cache smart photo and area reporting module

--- whatsup-dog/sw.js
+++ whatsup-dog/sw.js
@@ -1,5 +1,5 @@
-const CACHE='whatsup-dog-v9-nijkerk-digitized';
-const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./manifest.webmanifest','./icon.svg','./waldo-mark.png','./data/nijkerk-losloopgebieden.geojson'];
+const CACHE='whatsup-dog-v10-smart-report';
+const CORE=['./','./index.html','./styles.css?v=5','./home.css?v=5','./app.js?v=5','./official-areas.js?v=5','./home.js?v=5','./smart-report.css?v=1','./smart-report.js?v=1','./manifest.webmanifest','./icon.svg','./waldo-mark.png','./data/nijkerk-losloopgebieden.geojson'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
